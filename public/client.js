4ae82aae9bbd666b7a2f5ff3fa22fc489e720e0d
awwa500@gmail.com <awwa500@gmail.com>
2014-05-07 06:03:29 +0900

    Change image

--- public/client.js
+++ public/client.js
@@ -1,14 +1,23 @@
+var switcher = false;
+
 // connect to the websocket
 var socket = io.connect();
 
-var validColor = function (color) {
-  var elem = document.createElement("div");
-  elem.style.color = color;
-  return !!elem.style.color;
+var validColor = function(color) {
+    var elem = document.createElement("div");
+    elem.style.color = color;
+    return !!elem.style.color;
 };
 
 var showExample = function() {
-  document.getElementById("example").innerHTML = randomColor();
+    document.getElementById("example").innerHTML = randomColor();
+//    if (switcher) {
+//        document.getElementById("image").src = "http://image.itmedia.co.jp/news/articles/1405/06/yu_express1.jpg";
+//        switcher = false;
+//    } else {
+//        document.getElementById("image").src = "https://pbs.twimg.com/profile_images/2444266750/j3jlc3ktl6sl9ol2wvyi.jpeg";
+//        switcher = true;
+//    }
 };
 
 setInterval(showExample, 5000);
@@ -16,10 +25,12 @@ showExample();
 
 // listen for new colors from the server
 socket.on('input', function(input) {
-	var color = input.color;
-	var requester = input.requester;
-	document.body.style.backgroundColor = validColor(color) ? color : randomColor();
+    var color = input.color;
+    var requester = input.requester;
+    document.body.style.backgroundColor = validColor(color) ? color
+            : randomColor();
     document.getElementById("requester").innerHTML = requester;
+    document.getElementById("image").src = "img/image.jpg";
 });
 
 socket.emit('ready');
--- server.js
+++ server.js
@@ -4,7 +4,30 @@ var express = require("express.io");
 var shared = require("./public/shared");
 var util = require('util');
 
-//var authToken = process.env.AUTH_TOKEN || require("fs").readFileSync("authtoken.txt").toString().trim();
+// var authToken = process.env.AUTH_TOKEN ||
+// require("fs").readFileSync("authtoken.txt").toString().trim();
+
+var fs = require('fs'), path = require('path');
+
+var fileCopy = function(srcFile, dstFile, callback) {
+
+    if (fs.existsSync(srcFile) && fs.statSync(srcFile).isFile()) {
+        var oldFile = fs.createReadStream(srcFile);
+        var newFile = fs.createWriteStream(dstFile);
+
+        oldFile.addListener("data", function(chunk) {
+            newFile.write(chunk);
+        });
+        oldFile.addListener("close", function() {
+            newFile.end();
+            if (callback) {
+                callback();
+            }
+        });
+    } else {
+        throw 'file not found: ' + srcFile;
+    }
+};
 
 // create the express.io app
 var app = express();
@@ -20,36 +43,50 @@ app.use(express.static(process.cwd() + '/public'));
 // handle input
 app.post('/hook', function(req, res) {
 
-  // extract the color from the request
-  var color = (req.body.Body || req.body.text).split(/\n/)[0].toLowerCase().replace(/\s/g, "");
+    // extract the color from the request
+    var color = (req.body.Body || req.body.text).split(/\n/)[0].toLowerCase()
+            .replace(/\s/g, "");
 
-  // extract the requester mail address
-  var requester = req.body.from;
-  
-  // output request object
-  //console.log(util.inspect(req, false, null));
-  
-  var input = { color: color, requester: requester };
-  console.log(color);
-  console.log(requester);
+    // extract the requester mail address
+    var requester = req.body.from;
 
-  
-  // prepare the reply to Twilio
-  var resp = new twilio.TwimlResponse();
-  
-  // broadcast the color to all connected browsers
-  app.io.broadcast('input', input);
-  lastInput = input;
+    // output request object
+//    console.log(util.inspect(req, false, null));
 
-  // send the reply to Twilio
-  // SendGrid will just ignore the reply
-  res.send(resp.toString());
+    // copy file to the public
+    var file = null;
+    if (req.body.attachments > 0) {
+        var srcFile = req.files.attachment1.path;
+        // handle only jpg
+        if (path.extname(srcFile) == '.jpg') {
+            fileCopy(srcFile, './public/img/image.jpg');
+            file = './public/img/image.jpg';
+        }
+    }
+
+    var input = {
+        color : color,
+        requester : requester,
+        file : file
+    };
+    console.log(input);
+
+    // prepare the reply to Twilio
+    var resp = new twilio.TwimlResponse();
+
+    // broadcast the color to all connected browsers
+    app.io.broadcast('input', input);
+    lastInput = input;
+
+    // send the reply to Twilio
+    // SendGrid will just ignore the reply
+    res.send(resp.toString());
 });
 
 app.io.route('ready', function(req) {
-  if (lastInput) {
-    req.io.emit('input', lastInput);
-  }
+    if (lastInput) {
+        req.io.emit('input', lastInput);
+    }
 });
 
 app.listen(process.env.PORT || 7076);
